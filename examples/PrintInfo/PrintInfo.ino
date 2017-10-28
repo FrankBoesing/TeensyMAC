ff1e560f2def03a512ac2a7555e841de49010f01
Peter Newman <peterjnewman@gmail.com>
2017-10-28 17:36:59 +0100

    Add a method to return the MAC as an array for use with the Ethernet library

--- examples/PrintInfo/PrintInfo.ino
+++ examples/PrintInfo/PrintInfo.ino
@@ -8,4 +8,15 @@ void loop() {
   delay(500);
   Serial.printf("Serial: %u\n", teensySerial());
   Serial.printf("MAC: 0x%012llX\n", teensyMAC());
+
+  Serial.print("MAC as array: ");
+  uint8_t mac[6];
+  teensyMACArray(mac);
+  for (uint8_t i = 0; i < 6; ++i) {
+    Serial.printf("%02X", mac[i]);
+    if (i < 5) {
+      Serial.print(":");
+    }
+  }
+  Serial.println("");
 }
